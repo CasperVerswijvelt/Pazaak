af3cf6b3ca0d30c6e1b606166e8d091ee3d04573
Casper Verswijvelt <casper.verswijvelt.y7500@student.hogent.be>
2017-02-21 14:55:51 +0100

    Main ge-add

---
+++ Project-Pazaak/src/ui/Main.java
@@ -0,0 +1,16 @@
+/*
+ * This code was written by Casper Verswijvelt
+ * Any unauthorized use is illegal.
+ * © Casper Verswijvelt 2016-2017
+ */
+package ui;
+
+/**
+ *
+ * @author Casper
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
