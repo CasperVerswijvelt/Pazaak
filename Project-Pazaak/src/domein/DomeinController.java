bbc4ce1b835159f9541ca72ce3b46336da8fcf8b
Casper Verswijvelt <casper.verswijvelt.y7500@student.hogent.be>
2017-02-21 14:22:09 +0100

    Test: domeinpackage en DC

---
+++ Project-Pazaak/src/domein/DomeinController.java
@@ -0,0 +1,14 @@
+/*
+ * This code was written by Casper Verswijvelt
+ * Any unauthorized use is illegal.
+ * © Casper Verswijvelt 2016-2017
+ */
+package domein;
+
+/**
+ *
+ * @author Casper
+ */
+public class DomeinController {
+    
+}
