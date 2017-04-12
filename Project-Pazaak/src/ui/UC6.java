659bfe6d631807570fad44a41fed8d98c324c526
Casper Verswijvelt <casper.verswijvelt.y7500@student.hogent.be>
2017-04-12 13:29:23 +0200

    UC5 klaar

---
+++ Project-Pazaak/src/ui/UC6.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ui;
+
+import domein.DomeinController;
+import domein.Wedstrijd;
+import java.util.ResourceBundle;
+
+/**
+ *
+ * @author Casper
+ */
+class UC6 {
+
+    static void start(DomeinController dc, ResourceBundle r, Wedstrijd w) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
