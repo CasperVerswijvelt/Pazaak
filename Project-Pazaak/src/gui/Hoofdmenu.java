f903f8d749a7e02c342a7f1489c98cf190d3defb
Goran Vandenbossche <goran.vandenbossche.y0750@student.hogent.be>
2017-04-20 00:15:26 +0200

    Merge origin/master
    Conflicts:
    	Project-Pazaak/src/ui/Console.java
    	Project-Pazaak/src/ui/startUp.java

---
+++ Project-Pazaak/src/gui/Hoofdmenu.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import domein.DomeinController;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author goran
+ */
+public class Hoofdmenu extends GridPane{
+    
+    private DomeinController dc;
+    private ResourceBundle r;
+    private TaalSelectieScherm parent;
+    
+    private Label lblTitel;
+    private Button btnTitel;
+    private Button btnNieuweSpeler;
+    private Button btnNieuweWedstrijd;
+    private Button btnKoopKaart;
+    private Button btnLaadWedstrijd;
+    
+    public Hoofdmenu(TaalSelectieScherm parent, DomeinController dc, ResourceBundle r) {
+        this.parent = parent;
+        this.dc = dc;
+        this.r= r;
+        buildGUI();
+    }
+
+    private void buildGUI() {
+        this.setPadding(new Insets(10));
+        this.setVgap(20);
+        this.setHgap(10);
+        
+        lblTitel = new Label(r.getString("WELCOME"));
+        this.add(lblTitel,0,0,2,1);
+        //buttons
+        btnNieuweSpeler = new Button(r.getString("NEWPLAYEROPTION"));
+        btnNieuweWedstrijd = new Button(r.getString("STARTGAMEOPTION"));
+        btnKoopKaart = new Button(r.getString("BUYCARDOPTION"));
+        //toevoegen aan gridpane
+        this.add(btnNieuweSpeler,0,1);
+        this.add(btnNieuweWedstrijd,0,2);
+        this.add(btnKoopKaart,0,3);
+        
+        btnNieuweSpeler.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                toSpelerMenu();
+            }
+        });
+    }
+    private void toSpelerMenu() {
+        Spelermenu sm = new Spelermenu(this, dc, r);
+        Stage stage = (Stage) this.getScene().getWindow();
+        
+        Scene scene;
+        scene = new Scene(sm,400,500);
+        stage.setScene(scene);
+    }
+
+    public void zetTerugActief(Stage stage) {
+        stage.setScene(this.getScene());
+    }
+}
---
+++ Project-Pazaak/src/gui/TaalSelectieScherm.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import domein.DomeinController;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author goran
+ */
+public class TaalSelectieScherm extends GridPane{
+    private DomeinController dc = new DomeinController();
+    private ResourceBundle r;
+    
+    private Hoofdmenu hm;
+    
+    private Button btnNederlands, btnEnglish, btnFrancais;
+    
+    public TaalSelectieScherm(DomeinController dc){
+        this.dc = dc;
+        buildGUI();
+    }
+
+    private void buildGUI() {
+        this.setPadding(new Insets(10));
+        this.setVgap(20);
+        this.setHgap(10);
+        this.setAlignment(Pos.CENTER);
+        
+        btnNederlands = new Button("Nederlands");
+        btnEnglish = new Button("English");
+        btnFrancais = new Button("Francais");
+        
+        this.add(btnNederlands,0,0);
+        this.add(btnFrancais,0,1);
+        this.add(btnEnglish,0,2);
+        
+        btnNederlands.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent event) {
+                Locale l =  new Locale("nl", "BE");
+                r = ResourceBundle.getBundle("language/Language", l);
+                toMenu();
+            }
+        });
+        btnEnglish.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent event) {
+                Locale l =  new Locale("en", "GB");
+                r = ResourceBundle.getBundle("language/Language", l);
+                toMenu();
+            }
+        });
+        btnFrancais.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent event) {
+                Locale l =  new Locale("fr", "FR");
+                r = ResourceBundle.getBundle("language/Language", l);
+                toMenu();
+            }
+        });
+    }
+    
+    public void toMenu(){
+        Hoofdmenu hm = new Hoofdmenu(this, dc, r);
+        Stage stage = (Stage) this.getScene().getWindow();
+        
+        stage.setTitle("Pazaak");
+        
+        Scene scene;
+        scene = new Scene(hm,300,500);
+        stage.setScene(scene);
+    }
+}
