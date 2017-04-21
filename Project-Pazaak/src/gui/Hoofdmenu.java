825189c97dd9de3958e5837b582a97f1133325da
Goran Vandenbossche <goran.vandenbossche.y0750@student.hogent.be>
2017-04-21 14:23:57 +0200

    link gelegd tussen het hoofdmenu en het wedstrijdscherm

--- Project-Pazaak/src/gui/Hoofdmenu.java
+++ Project-Pazaak/src/gui/Hoofdmenu.java
@@ -67,7 +67,12 @@ public class Hoofdmenu extends GridPane{
             public void handle(ActionEvent event) {
                 toKaartWinkel();
             }
-
+        });
+        btnNieuweWedstrijd.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                toWedstrijdHoofdScherm();
+            }
         });
     }
     private void toSpelerMenu() {
@@ -85,6 +90,15 @@ public class Hoofdmenu extends GridPane{
 
         Scene scene;
         scene = new Scene(kw, 400, 500);
+        stage.setScene(scene);
+    }
+
+    private void toWedstrijdHoofdScherm() {
+        WedstrijdHoofdScherm whs = new WedstrijdHoofdScherm(this, dc, r);
+        Stage stage = (Stage) this.getScene().getWindow();
+
+        Scene scene;
+        scene = new Scene(whs, 400, 500);
         stage.setScene(scene);
     }
 
--- Project-Pazaak/src/main/startUpGui.java
+++ Project-Pazaak/src/main/startUpGui.java
@@ -7,11 +7,8 @@ package main;
 
 import domein.DomeinController;
 import gui.TaalSelectieScherm;
-import java.util.ResourceBundle;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -20,8 +17,7 @@ import javafx.stage.Stage;
  * @author goran
  */
 public class startUpGui extends Application {
-private ResourceBundle r;
-    
+   
     @Override
     public void start(Stage stage) throws Exception {
         DomeinController dc = new DomeinController();
