214b2c55212d9661ec0afb0eb8a4a764e2fd0735
Casper Verswijvelt <casper.verswijvelt.y7500@student.hogent.be>
2017-04-23 18:28:21 +0200

    kaartenpaneel spelbord

--- Project-Pazaak/src/gui/KaartenPaneel.java
+++ Project-Pazaak/src/gui/KaartenPaneel.java
@@ -7,16 +7,61 @@ package gui;
 
 import domein.DomeinController;
 import java.util.ResourceBundle;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
 /**
  *
  * @author goran
  */
-class KaartenPaneel extends GridPane{
+class KaartenPaneel extends GridPane {
 
-    KaartenPaneel(SpelBordPaneel aThis, DomeinController dc, ResourceBundle r) {
-        
+    private SpelBordPaneel parent;
+    private DomeinController dc;
+    private ResourceBundle r;
+
+    KaartenPaneel(SpelBordPaneel parent, DomeinController dc, ResourceBundle r) {
+        this.dc = dc;
+        this.parent = parent;
+        this.r = r;
+
+        buildGUI();
+
     }
-    
+
+    private void buildGUI() {
+        for (int rij = 0; rij < 3; rij++) {
+            for (int kolom = 0; kolom < 3; kolom++) {
+                Button button = new Button();
+                button.setMinSize(50, 80);
+                button.setDisable(true);
+                this.add(button, kolom, rij);
+            }
+        }
+        this.setHgap(10);
+        this.setVgap(10);
+        laadSpelbord(dc.geefStartStapel("Bob"));
+    }
+
+    public void laadSpelbord(String[][] spelbord) {
+        int rij = 0;
+        int kolom = 0;
+        for (int i = 0; i < spelbord.length; i++) {
+
+            Button button = new Button();
+            button.setMinSize(50, 80);
+            String[] kaart = Utilities.veranderNaarMooieLayout(spelbord[i]);
+            button.setText(kaart[0] + kaart[1]);
+
+            this.add(button, kolom, rij);
+            kolom++;
+            if(kolom > 2) {
+                if(++rij >2)
+                    return;
+                kolom=0;
+            }
+
+        }
+    }
+
 }
