3a8cd8453b3f363c332e089c7554b87739f150d4
Casper Verswijvelt <casper.verswijvelt.y7500@student.hogent.be>
2017-04-28 09:18:05 +0200

    fix

--- Project-Pazaak/src/guiSceneBuilder/SetSpeelScherm.java
+++ Project-Pazaak/src/guiSceneBuilder/SetSpeelScherm.java
@@ -31,7 +31,7 @@ import javafx.stage.Stage;
  * @author goran
  */
 public class SetSpeelScherm extends GridPane {
-    
+
     private BorderPaneController parent;
     private DomeinController dc;
     private ResourceBundle r;
@@ -151,7 +151,7 @@ public class SetSpeelScherm extends GridPane {
                             try {
                                 dc.slaWedstrijdOp(naam.get());
                                 parent.naarMenu();
-                                break;
+
                             } catch (GameAlreadyExistsException e) {
                                 gameExistsAlert.showAndWait();
                                 continue;
@@ -159,6 +159,8 @@ public class SetSpeelScherm extends GridPane {
                                 DBAlert.showAndWait();
                                 continue;
                             }
+                            System.out.println("nu zou hij moeten breaken uit de loop");
+                            break;
 
                         } else {
                             setTenEinde();
